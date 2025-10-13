d67553450a6e26a865fb7621bcf1914af9505fbb
Hyperswap <hyperswapx@gmail.com>
2025-10-13 18:57:08 +0200

    constant changed

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const USER_PROXY_FACTORY_ADDRESS = '0x5c523a1cD987181CA86F4E20AefA8A961cDEecD9';
 // export const SwapRouter02ExecutorAddress = '0x87ac8F4aD3BaB7c213d106BDa1Fc2B1759379868'
-export const SwapRouter02ExecutorAddress = '0xF7e98Bfa59926caa05E7E632BE8b8C4a5f8caA3C' // EXclusive
+export const SwapRouter02ExecutorAddress = '0xa9b075d37ce66cD53661ca0e268636a191d2E594' // EXclusive
 
 // export const SwapRouter02ExecutorAddress = '0x49A3bF8708DA69CE15C33ac09EFD8f7c522FF346' //TEST
